ab66c7684228b43e9bb7d4e882dc44f5dd1651ba
blessed <sibandablessed724@gmail.com>
2024-11-23 14:52:46 +0200

    Update Navbar.tsx
    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,10 @@
-"use client";
+
+
+
+
+
+
+"use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import {
@@ -9,36 +15,35 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 
+
 import Link from "next/link";
 import { MapPin, Menu, Search, ShoppingCart, User } from 'lucide-react';
 import Image from "next/image";
 import SearchBar from "./SearchBar";
 import dynamic from "next/dynamic";
 import MenuPhone from "./Menu";
-import { GetServerSideProps } from "next";
-import cookies from "next-cookies";
 
 const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
-interface NavbarProps {
-  initialRefreshToken: string;
-}
-
-const Navbar = ({ initialRefreshToken }: NavbarProps) => {
+const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
-          <Image src="/logo.jpg" alt="" width={124} height={124} />
-          <div className="text-xl tracking-wide hidden md:flex">MON</div>
+        <Image src="/logo.jpg" alt="" width={124} height={124} />
+            <div className="text-xl tracking-wide hidden md:flex">MON </div>
+        
         </Link>
 
         {/* RIGHT */}
-        <div className="w-full justify-end flex items-end gap-8 md:pl-[3rem]">
-          <NavIcons initialRefreshToken={initialRefreshToken} />
+        <div className="w-full justify-end flex items-end  gap-8 md:pl-[3rem]">
+        
+          <NavIcons />
           <MenuPhone />
         </div>
+
+      
       </div>
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
@@ -46,19 +51,20 @@ const Navbar = ({ initialRefreshToken }: NavbarProps) => {
         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.jpg" alt="" width={64} height={64} />
-            <div className="text-xl tracking-wide">MON</div>
+            <div className="text-xl tracking-wide ">MON </div>
           </Link>
           <div className="hidden xl:flex gap-6 md:gap-9 md:pl-[3rem]">
             <Link href="/">Homepage</Link>
             <Link href="/">Shop</Link>
             <Link href="/">Deals</Link>
             <Link href="/about-us">About</Link>
+            
           </div>
         </div>
         {/* RIGHT */}
         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8 md:pl-[3rem]">
           <SearchBar />
-          <NavIcons initialRefreshToken={initialRefreshToken} />
+          <NavIcons />
         </div>
       </div>
     </div>
@@ -66,93 +72,6 @@ const Navbar = ({ initialRefreshToken }: NavbarProps) => {
 };
 
 export default Navbar;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { req } = context;
-  const allCookies = cookies(context);
-
-  const refreshToken = allCookies.refreshToken;
-
-  if (!refreshToken) {
-    return { redirect: { destination: "/login", permanent: false } };
-  }
-
-  return {
-    props: { initialRefreshToken: refreshToken },
-  };
-};
-
-
-
-
-// "use client"
-// import { Button } from "@/components/ui/button";
-// import { Card, CardContent, CardFooter } from "@/components/ui/card";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// import { Input } from "@/components/ui/input";
-
-
-// import Link from "next/link";
-// import { MapPin, Menu, Search, ShoppingCart, User } from 'lucide-react';
-// import Image from "next/image";
-// import SearchBar from "./SearchBar";
-// import dynamic from "next/dynamic";
-// import MenuPhone from "./Menu";
-
-// const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
-
-// const Navbar = () => {
-//   return (
-//     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-//       {/* MOBILE */}
-//       <div className="h-full flex items-center justify-between md:hidden">
-//         <Link href="/">
-//         <Image src="/logo.jpg" alt="" width={124} height={124} />
-//             <div className="text-xl tracking-wide hidden md:flex">MON </div>
-        
-//         </Link>
-
-//         {/* RIGHT */}
-//         <div className="w-full justify-end flex items-end  gap-8 md:pl-[3rem]">
-        
-//           <NavIcons />
-//           <MenuPhone />
-//         </div>
-
-      
-//       </div>
-//       {/* BIGGER SCREENS */}
-//       <div className="hidden md:flex items-center justify-between gap-8 h-full">
-//         {/* LEFT */}
-//         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
-//           <Link href="/" className="flex items-center gap-3">
-//             <Image src="/logo.jpg" alt="" width={64} height={64} />
-//             <div className="text-xl tracking-wide ">MON </div>
-//           </Link>
-//           <div className="hidden xl:flex gap-6 md:gap-9 md:pl-[3rem]">
-//             <Link href="/">Homepage</Link>
-//             <Link href="/">Shop</Link>
-//             <Link href="/">Deals</Link>
-//             <Link href="/about-us">About</Link>
-            
-//           </div>
-//         </div>
-//         {/* RIGHT */}
-//         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8 md:pl-[3rem]">
-//           <SearchBar />
-//           <NavIcons />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
 
 
 
