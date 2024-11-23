7e1fcd0f033e45704fbf3fc037dc1a1438a34788
blessed <sibandablessed724@gmail.com>
2024-11-23 15:26:07 +0200

    Update NavIcons.tsx
    cagagss

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -20,9 +20,6 @@ const NavIcons = () => {
   const wixClient = useWixClient();
   const isLoggedIn = wixClient.auth.loggedIn();
 
-  // TEMPORARY
-  // const isLoggedIn = false;
-
   const handleProfile = () => {
     if (!isLoggedIn) {
       router.push("/login");
@@ -30,22 +27,6 @@ const NavIcons = () => {
       setIsProfileOpen((prev) => !prev);
     }
   };
-
-  // AUTH WITH WIX-MANAGED AUTH
-
-  // const wixClient = useWixClient();
-
-  // const login = async () => {
-  //   const loginRequestData = wixClient.auth.generateOAuthData(
-  //     "http://localhost:3000"
-  //   );
-
-  //   console.log(loginRequestData);
-
-  //   localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
-  //   const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
-  //   window.location.href = authUrl;
-  // };
 
   const handleLogout = async () => {
     setIsLoading(true);
@@ -55,7 +36,6 @@ const NavIcons = () => {
     setIsProfileOpen(false);
     router.push(logoutUrl);
   };
-
 
   const { cart, counter, getCart } = useCartStore();
 
@@ -71,7 +51,6 @@ const NavIcons = () => {
         width={22}
         height={22}
         className="cursor-pointer"
-        // onClick={login}
         onClick={handleProfile}
       />
       {isProfileOpen && (
@@ -98,7 +77,7 @@ const NavIcons = () => {
           {counter}
         </div>
       </div>
-      {isCartOpen && <CartModal />}
+      {isCartOpen && <CartModal onClose={() => setIsCartOpen(false)} />}
     </div>
   );
 };
