9eeb54717ca73baceeec05edb84b181d7a3abaf2
blessed <sibandablessed724@gmail.com>
2024-11-26 15:11:26 +0200

    Update page.tsx
    iamge sizes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -31,7 +31,7 @@ export default async function SinglePage({ params }: { params: Params }) {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
-      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+      <div className="w-full md:w-1/2 lg:sticky top-20 h-1/2">
         <ProductImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
