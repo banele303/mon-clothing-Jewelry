b163cc3085b94ac855da31d57abcea631040a268
blessed <sibandablessed724@gmail.com>
2024-11-26 15:35:59 +0200

    Update ProductImages.tsx
    small image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -39,7 +39,7 @@ const ProductImages = ({ items }: { items: any }) => {
       <div className="flex justify-between gap-4 mt-8">
         {items.map((item:any, i:number) => (
           <div
-            className="w-1/4 h-30 relative gap-4 mt-4 cursor-pointer"
+            className="w-1/4 h-32 relative gap-4 mt-4 cursor-pointer"
             key={item._id}
             onClick={() => setIndex(i)}
           >
